41f65c1dfe52115eb9c8bdbb3fbc20b2ee1b92ad
black <black@example.com>
2026-07-04 21:59:37 +0500

    Call greeting functions in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     print("hello everyone")
+    greetWhite()
+    greetBlack()
 
 def greetBlack():
     print("hello black")
+
+def greetWhite():
+    print("hello white")
 
 if __name__ == "__main__":
     main()
